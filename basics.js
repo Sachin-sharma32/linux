b866fe50ae3cee1d3f68765e96565d7c5c68fb30
sachin <sachin2sharma003@gmail.com>
2023-07-25 16:22:08 +0530

    s-2

---
+++ basics.js
@@ -0,0 +1,65 @@
+// TOPIC: CHAPTER 1
+
+// SUB: USES
+
+// asure
+// google cloud
+// facebook website
+// Stock exchanges around the world
+
+// SUB: What is linux
+
+// OS 
+
+// SUB: Features of OS
+
+// Detecting and preparing hardware
+// Manage Process -> where and how much CPU of allocate to each program
+// Managing RAM
+// UI
+// Filesystem 
+// Linux is based on UNIX
+
+// SUB: Advanced Features
+
+// Clustering -> combining multiple systems into one
+// Virtualization 
+// Cloud Computing
+// Real Time Computing -> prioritising processes
+
+// SUB: Propritary OS
+
+// windows and Mac 
+// cannot and see and change code of OS
+// cannot plug your own software into OS
+
+// SUB: History of linux 
+
+// UNIX was owned by AT&t, initially they kept it open sourced
+// C was created as a high level language to make programs for UNIX
+// AT&T split up and were not allowed to sell software 
+// So they licenced it to universities, but this version lacked build in compiler
+// University of California created a similar UNIX OS and was open sourced called Berkeley Software Distribution (BSD)
+// AT&T's version became more restrictive
+// At&T's UNIX falied because of stiff compition from already established Windows
+// different companies started their own linux distrubutions from BSD which were private 
+// GNU was the first development which started a "Free Software Foundation"
+// free software word was replaced by "open source"
+// GNU was better than original UNIX because of being open sourced
+// GPL (GNU Public Lience) was obtained to keep GNU open sourced
+// But GNU failed to produce a kernel 
+// BSD falied because of law-suits from AT&T 
+// MacOS is build on BSD 
+// Linus Torvelds started Linux development as a kernel for GNU 
+// Linux Foundation was estabilished to protech Linux legally and set standards
+// With KDE and GNOME, linux became available for desktop 
+// Open Source Initiative (OSI) issue licences to software it accespt -> GPL, Mozilla, MIT, BSD licences as well 
+
+// SUB: Linux Distros emergence
+
+// to bring linux to normal users
+// Linux kernel + softwares + UI + GUI -> distribution 
+// Red Hat Linux and Debian laid foundation for other distributions 
+// Red Hat was equired by IBM and Red Hat Linux was abandoned and Red Hat Enterprise Linux and Fedora were formed
+// RHEL -> enterprise level
+// Fedora -> normal users
