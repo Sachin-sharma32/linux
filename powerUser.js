145e5952cf72ae46fdb534a1a0d39c41904bdeac
Sachin-sharma32 <sachin2sharma001@gmail.com>
2023-07-31 08:04:25 +0530

    s-5

--- powerUser.js
+++ powerUser.js
@@ -1,5 +1,7 @@
 // TOPIC: SHELL
 
+// TODO: all tables (eg. TABLE 1.5)
+
 // most linux distro comes with bash shell by default (Bourne again shell -> named after Bounce shell from UNIX)
 // 3 ways to access shell -> shell prompt, terminal, virtual console
 // most distros uses GNOME terminal by default
@@ -88,18 +90,84 @@
 
 // .bash_history (where all history of commands is stored)
 
-// SUB: Connecting and expanding command
+// SUB: Connecting command
 
 // Pipe | (connect output of one command to input of other)
 // TERM: ls /usr/bin | sort -f | less 
 
 // Sequencial commnd ; (run commands one after another)
-// date ; troff -me {filename} | 1pr ; date (troff -> format file, 1pr -> print the output of troff)
+// TERM: date ; troff -me {filename} | 1pr ; date (troff -> format file, 1pr -> print the output of troff)
 
 // Background command &
-// troff -m {filename} &  (format file in background and not bloack the shell)
+// TERM: troff -m {filename} &  (format file in background and not bloack the shell)
 
-// command substitution $() -> allowing output of command taken as argument by shell itself instead of other command
-// nvim $(find /home | grep xyz)  
+// SUB: Expanding command
 
+// command substitution $() or "" -> allowing output of command taken as argument by shell itself instead of other command
+// TERM: nvim $(find /home | grep xyz)  -> open file containing xyz inside /home then open it in nvim  
+
+// Expanding arathmethic operations $() or $[]
+// TERM: echo "I am $[90-78] years old"
+// TERM: echo "There are $(ls | wc -w) files in this folder"  -> wc (count)
+
+// Expanding environment variables $
+// TERM: ls -l $BASH  -> -l (long format)
+
+// SUB: Shell variables
+
+// variables -> can only be used in shell in which they are defined  
+// environment variable -> can be used in other shells 
+// TERM: env 
+// TERM: echo ${env name} 
+
+// TERM: alias r='rm -i'
+// TERM: alias
+// TERM: export PROMPT_COMMAND='date' (export -> set env variable, PROMPT_COMMAND= env variable which display something after typing every command)
+// TERM: export TMOUT='20' (exit the shell after 20s if left idle)
+
+// SUB: Creating Shell environment
+
+// to change /etc/profile or /etc/bashrc you must be root user
+// /etc/profile.d/custom.sh -> add  settings instead of editing the actual file /etc/profile
+
+// $HOME/.profile, $HOME/.bashrc  -> user specific settings
+
+// to persist for eg. alias put it inside .bashrc of bashrc file (global) 
+// nvim $HOME/.bashrc -> alias d='date'
+
+// in shell before $ or ~ -> the name of current user followed by current directory
+// .bashrc -> export PS1="[\t \w]\$ "   (change the look before "$" in terminal, \t -> time, \w -> full path, \$ -> $)
+// .bashrc -> TMOUT=30  (set env variable, TMOUT -> close shell after 30s of idle time)
+// .bashrc -> PATH=$PATH:/getstuff/bin ; export PATH   (add /getstuff/bin to PATH, $PATH -> read the current path)
+
+// .bashrc -> M=/work/time/files/info/memos ; export M   (now you can access this folder by $M, eg. cd $M )
+
+// ls --help (or -h)
+
+// man ls (info about command)
+// section 1 of man page -> for regular users
+// section 5,8,4 -> for administrators
+// section 2,3 -> for programmers
+// man 5 ls (see section 5 of man page)
+
+// TOPIC: FILESYSTEM
+
+// filesystem -> where all info about computer is stored
+// "/" -> root directory (not root user)
+
+// SUB: Linux vs mac-windows filesystem
+
+// "\" -> Linux, "/" -> MAC
+// 3 character or less suffix have no meaning in linux eg. .txt, only the "permissions" of file specfies it's executable nature
+// linux files and directories have permissions and ownerships attached
+
+// when you open a shell you are placed in "home" directory
+// cd /usr/home (starts with a / so "absolute path")
+// cd ~ (home directory)
+// mkdir {folder}
+// ls -ld {folder} (d -> directory)
+// chmod 700 {folder} (chmod -> change permission, 700 -> give you full access other's has no access)
+// touch {filename}
+
+// SUB: Metacharacters 
 
